035b877c58ffe55d945b91e51c0e996c1c83930b
josegrego00 <josegrego00col@gmail.com>
2026-02-27 19:36:58 -0500

    Creacion de entidades Tenant y Usuario

---
+++ server/src/main/java/com/cobranzasapi/saas/models/Tenant.java
@@ -0,0 +1,30 @@
+package com.cobranzasapi.saas.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Tenant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombreTenant;
+
+    @Column(unique = true)
+    private String subdominio;
+
+    private Boolean activo;
+
+}
---
+++ server/src/main/java/com/cobranzasapi/saas/models/Usuario.java
@@ -0,0 +1,39 @@
+package com.cobranzasapi.saas.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String rol;
+
+    @Column(nullable = false)
+    private Long tenantId;
+
+}
