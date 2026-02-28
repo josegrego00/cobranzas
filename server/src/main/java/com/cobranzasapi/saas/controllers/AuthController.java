48c7646404cffe47b640710bac2a839d8dbb3687
johanriascos7777 <johanriascos7777@gmail.com>
2026-02-28 15:14:45 -0500

    feat: Add testing controller and testing .jsx

---
+++ server/src/main/java/com/cobranzasapi/saas/controllers/AuthController.java
@@ -0,0 +1,29 @@
+package com.cobranzasapi.saas.controllers;
+/* 
+import com.cobranzasapi.saas.models.User;
+import com.cobranzasapi.saas.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth") // Prefijo de la ruta (Como en routes/index.js)
+@CrossOrigin(origins = "http://localhost:5173") // Permite que React se conecte
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    // POST http://localhost:8080/api/auth/register
+    @PostMapping("/register")
+    public ResponseEntity<?> registerHandler(@RequestBody User user) {
+        try {
+            User newUser = authService.registerUser(user);
+            return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error al registrar: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+}
+     */
